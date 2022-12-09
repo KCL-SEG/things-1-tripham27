a313fb83b87a78859f21fbfb1b0e60c8c872ae28
Tri <K21003770@kcl.ac.uk>
2022-12-09 03:22:21 +0000

    fix view

--- things/views.py
+++ things/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    response = HttpResponse("Welcome")
-    return response 
+    return render(request, 'home.html')
