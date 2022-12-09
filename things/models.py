004f820a4a6a2b149af275d05d5769fe370a3ad9
Tri <K21003770@kcl.ac.uk>
2022-12-09 03:46:26 +0000

    idk

--- things/models.py
+++ things/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
-class User(AbstractUser):
+class Thing(models.Model):
     name = models.TextField(max_length=30,unique=True,blank=False)
     description = models.TextField(max_length=120,unique=True,blank=False)
     quantity = models.IntegerField(unique=False,validators=[MinValueValidator(0),MaxValueValidator(100)])
